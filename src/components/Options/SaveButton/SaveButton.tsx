2f57b077a092b70d078d87417a6e419fa3292fe3
Alex Leung <alexleung25@outlook.com>
2025-04-23 18:32:59 -0700

    Add loading indicator for saving and error save alert

--- src/components/Options/SaveButton/SaveButton.tsx
+++ src/components/Options/SaveButton/SaveButton.tsx
@@ -13,6 +13,7 @@ import Snackbar from '@mui/material/Snackbar';
 import Tooltip from '@mui/material/Tooltip';
 import { platform } from '../../../utils/platform';
 import { urlPatterns } from '../../../utils/url-patterns';
+import CircularProgress from '@mui/material/CircularProgress';
 
 import type { UrlData } from '../../../types/url-data';
 
@@ -20,8 +21,9 @@ const SaveButton = ({ isPressedKeySave }: { isPressedKeySave: boolean }) => {
   const { editor } = useContext(MonacoEditorContext);
 
   const [saveDialogOpen, setSaveDialogOpen] = useState(false);
-
   const [saveAlert, setSaveAlert] = useState(false);
+  const [errorSaveAlert, setErrorSaveAlert] = useState(false);
+  const [saveLoading, setSaveLoading] = useState(false);
 
   useEffect(() => {
     // save key is pressed
@@ -39,43 +41,57 @@ const SaveButton = ({ isPressedKeySave }: { isPressedKeySave: boolean }) => {
   };
 
   const saveButtonContinue = async () => {
-    setSaveDialogOpen(false);
+    setSaveLoading(true);
 
-    if (editor) {
-      // current editor value
-      const editorValue: string = editor.getValue();
+    setTimeout(() => {
+      if (editor) {
+        // current editor value
+        const editorValue: string = editor.getValue();
 
-      // all urls from the editor regardless if they are valid or invalid
-      const editorUrls: string[] = editorValue.split('\n');
+        // all urls from the editor regardless if they are valid or invalid
+        const editorUrls: string[] = editorValue.split('\n');
 
-      // array containing all possible valid urls
-      const validUrls: string[] = editorUrls.filter((url: string): boolean => isValidUrl(url));
+        // array containing all possible valid urls
+        const validUrls: string[] = editorUrls.filter((url: string): boolean => isValidUrl(url));
 
-      // size of valid urls array
-      const validUrlsSize: number | null = Object.keys(
-        validUrls.filter(
-          (url: string) =>
-            // comments are filtered out
-            !url.startsWith(urlPatterns.doubleSlashCommentStr) &&
-            !url.startsWith(urlPatterns.hashCommentStr)
-        )
-      ).length;
+        // size of valid urls array
+        const validUrlsSize: number | null = Object.keys(
+          validUrls.filter(
+            (url: string) =>
+              // comments are filtered out
+              !url.startsWith(urlPatterns.doubleSlashCommentStr) &&
+              !url.startsWith(urlPatterns.hashCommentStr)
+          )
+        ).length;
 
-      // object of url related data to be stored
-      const urlData: UrlData = {
-        editorValue: editorValue,
-        validUrls: validUrls,
-        validUrlsSize: validUrlsSize,
-      };
+        // object of url related data to be stored
+        const urlData: UrlData = {
+          editorValue: editorValue,
+          validUrls: validUrls,
+          validUrlsSize: validUrlsSize,
+        };
 
-      chrome.storage.local.set(urlData);
-
-      setSaveAlert(true);
-    }
+        chrome.storage.local
+          .set(urlData)
+          .then(() => {
+            setSaveDialogOpen(false);
+            setSaveAlert(true);
+          })
+          .catch((err) => {
+            console.error('[Chrome Storage]: ', err);
+            setSaveDialogOpen(false);
+            setErrorSaveAlert(true);
+          })
+          .finally(() => {
+            setSaveLoading(false);
+          });
+      }
+    }, 250);
   };
 
   const alertOnClose = () => {
     setSaveAlert(false);
+    setErrorSaveAlert(false);
   };
 
   const saveKeyTitle: string = `Save ${
@@ -111,11 +127,15 @@ const SaveButton = ({ isPressedKeySave }: { isPressedKeySave: boolean }) => {
       <Dialog open={saveDialogOpen} onClose={saveButtonClose} disableRestoreFocus>
         <DialogTitle>Save All URLs?</DialogTitle>
         <DialogContent>
-          <DialogContentText>Are you sure you want to save all valid URLs?</DialogContentText>
+          <DialogContentText>Are you sure you want to save all URLs?</DialogContentText>
         </DialogContent>
         <DialogActions>
-          <Button onClick={saveButtonClose}>Cancel</Button>
-          <Button onClick={async () => saveButtonContinue()}>Save</Button>
+          <Button onClick={saveButtonClose} disabled={saveLoading}>
+            Cancel
+          </Button>
+          <Button onClick={saveButtonContinue} disabled={saveLoading}>
+            {saveLoading ? <CircularProgress size={24} /> : 'Save'}
+          </Button>
         </DialogActions>
       </Dialog>
       {saveAlert ? (
@@ -131,6 +151,22 @@ const SaveButton = ({ isPressedKeySave }: { isPressedKeySave: boolean }) => {
             sx={{ backgroundColor: '#8bc34a', color: '#000000' }}
           >
             Successfully saved URLs
+          </Alert>
+        </Snackbar>
+      ) : null}
+      {errorSaveAlert ? (
+        <Snackbar
+          autoHideDuration={3500}
+          open={errorSaveAlert}
+          onClose={alertOnClose}
+          disableWindowBlurListener={true}
+        >
+          <Alert
+            icon={false}
+            onClose={alertOnClose}
+            sx={{ backgroundColor: '#ef5350', color: '#000000' }}
+          >
+            Error: Unable to save URLs
           </Alert>
         </Snackbar>
       ) : null}
